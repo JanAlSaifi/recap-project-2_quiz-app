5fb84c7cf18016c7a8b6aae777c59679722a94f7
Your NameJanAlSaifi <jan.al.saifi@googlemail.com>
2024-11-08 11:38:55 +0100

    added const and eventlistener to index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // phew… not a lot going on here. Please add some code!
+console.clear();
+
+// --- Bookmarks Toogle --- //
+const cardBookmark = document.querySelector('[data-js="cardBookmark"]');
+
+cardBookmark.addEventListener("click", () => {
+  cardBookmark.classList.toggle("bookmark--active");
+});
+
+// --- Answer Toogle --- //
+const cardToogle = document.querySelector('[data-js="cardToogle"]');
+const cardAnswer = document.querySelector('[data-js="cardAnswer"]');
+
+cardToogle.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer--active");
+});
